a2a42c6bbc5fb20df4d9431af34f0014de74106e
superkang <superkang@v1040-wn-rt-b-37-123.campus-dynamic.uwaterloo.ca>
2022-06-08 15:06:56 -0400

    some changes on rank

--- rank.cc
+++ rank.cc
@@ -1,7 +1,26 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main()
 {
-    cout<<"Hello World Andy"<<endl;
+    vector<int> a,r; int n;
+    while(cin >> n)
+    {
+       a.push_back(n);
+       r.push_back(0);
+    }
+    for(int i = 1; i < a.size();i++)
+    {
+        for(int j = 0;j < i; j++)
+        {
+            if(a[i] >= a[j]) r[i]++;
+            else r[j]++;
+        }
+    }
+    for(auto i : r)
+    {
+        cout << i << endl;
+    }
+    return 0;
 }
